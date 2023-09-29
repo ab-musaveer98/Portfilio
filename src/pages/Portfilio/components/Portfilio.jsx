ca4dd3ee391b42eca398b48ee48f569cc0afec13
Ather73 <athershaikh03@gmail.com>
2023-09-29 18:47:18 +0530

    heading added

--- src/pages/Portfilio/components/Portfilio.jsx
+++ src/pages/Portfilio/components/Portfilio.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import HeroCard from "./HeroCard";
 
 const Portfilio = () => {
-  return <></>;
+  return <h1>remove this with your components</h1>;
 };
 
 export default Portfilio;
